1f6cbe7ce7a62053561ff0084f197498eaa384c8
Sion Shin <kmicety1@gmail.com>
2024-05-25 16:05:27 +0900

    project initialize

---
+++ src/main/java/com/savemyreceipt/smr/SmrApplication.java
@@ -0,0 +1,13 @@
+package com.savemyreceipt.smr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savemyreceipt/smr/SmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savemyreceipt.smr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
