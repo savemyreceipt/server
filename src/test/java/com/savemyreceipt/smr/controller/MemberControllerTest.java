6aede201edb1c957a020addb56990d5f84403210
Sion Shin <kmicety1@gmail.com>
2024-06-02 23:58:51 +0900

    test: MemberControllerTest 추가

--- src/test/java/com/savemyreceipt/smr/controller/MemberControllerTest.java
+++ src/test/java/com/savemyreceipt/smr/controller/MemberControllerTest.java
@@ -4,30 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.savemyreceipt.smr.DTO.member.request.MemberRequestDto;
 import com.savemyreceipt.smr.DTO.member.response.MemberResponseDto;
 import com.savemyreceipt.smr.exception.SuccessStatus;
-import com.savemyreceipt.smr.infrastructure.GroupRepository;
-import com.savemyreceipt.smr.infrastructure.MemberRepository;
-import com.savemyreceipt.smr.infrastructure.ReceiptRepository;
-import com.savemyreceipt.smr.oauth.OAuth2UserService;
-import com.savemyreceipt.smr.service.GroupService;
 import com.savemyreceipt.smr.service.MemberService;
-import com.savemyreceipt.smr.service.ReceiptService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -35,8 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
