4755e0b6341cd415eb1591eda01483bd5022c648
Sion Shin <kmicety1@gmail.com>
2024-06-02 10:07:55 +0900

    chore: CORS configuration 추가

--- src/main/java/com/savemyreceipt/smr/config/SecurityConfig.java
+++ src/main/java/com/savemyreceipt/smr/config/SecurityConfig.java
@@ -49,7 +49,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
             .headers(c -> c.frameOptions(FrameOptionsConfig::disable).disable())
             .oauth2Login(oauth2Login -> oauth2Login
                 .successHandler(oAuth2LoginSuccessHandler) // OAuth2 로그인 성공 핸들러
@@ -68,20 +67,6 @@ public class SecurityConfig {
             ).sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // 모든 출처 허용
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH")); // 허용할 HTTP 메소드
-        configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
-        configuration.setAllowCredentials(true); // 쿠키를 포함한 요청 허용
-        configuration.setMaxAge(3600L); // pre-flight 요청의 결과를 캐시하는 시간(초 단위)
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 CORS 설정 적용
-        return source;
     }
 
 }
