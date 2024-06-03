1897fb9c575354fca0782d9f92e774e1666a0ff8
Sion Shin <kmicety1@gmail.com>
2024-06-03 13:37:29 +0900

    feat: notification 추가

---
+++ src/main/java/com/savemyreceipt/smr/infrastructure/NotificationRepository.java
@@ -0,0 +1,23 @@
+package com.savemyreceipt.smr.infrastructure;
+
+import com.savemyreceipt.smr.domain.Member;
+import com.savemyreceipt.smr.domain.Notification;
+import com.savemyreceipt.smr.exception.ErrorStatus;
+import com.savemyreceipt.smr.exception.model.CustomException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByMemberAndCheckedFalse(Member member);
+
+    Optional<Notification> findById(Long id);
+
+    default Notification getNotificationById(Long id) {
+        return findById(id).orElseThrow(
+            () -> new CustomException(ErrorStatus.NOTIFICATION_NOT_FOUND, ErrorStatus.NOTIFICATION_NOT_FOUND.getMessage())
+        );
+    }
+
+}
